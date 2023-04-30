c84dab01d780a7a064b39ce6967033555ed4d73c
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-05-01 00:39:07 +0600

    cahg

--- src/object.ts
+++ src/object.ts
@@ -1,4 +1,11 @@
-const user = {
+const user: {
+    name: string,
+    age: number,
+    isMarried: boolean,
+    wife?: string
+} = {
     name: 'Montu mia',
-    age: 54
-}
+    age: 54,
+    isMarried: true,
+    wife: 'Bibi'
+};
