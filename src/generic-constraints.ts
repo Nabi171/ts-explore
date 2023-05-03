4028ee3d73b10ac6107d41f0e3f8490e489eb58c
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-05-03 21:25:44 +0600

    change in data

--- src/generic-constraints.ts
+++ src/generic-constraints.ts
@@ -1,16 +1,26 @@
-
-
-const addMeInMyCrushMind = <T>(myInfo: T) => {
+type MandatoryTypes = { name: string, age: number, salary: number }
+interface MandatoryInterface { name: string, age: number, salary: number }
+const addMeInMyCrushMind = <T extends MandatoryInterface>(myInfo: T) => {
     const crush = 'kate winslet'
     const newData = { ...myInfo, crush };
     return newData;
 };
 
-const myInfo = {
+
+type MyInfoType = {
+    name: string;
+    age: number;
+    salary: number;
+    other1: boolean,
+    other2: null;
+}
+const myInfo: MyInfoType = {
     name: "Persian",
     age: 100,
     salary: 100000,
+    other1: false,
+    other2: null,
 }
 
 const result5 = addMeInMyCrushMind(myInfo);
-
+result5
---
+++ src/generic-keyof-constraints.ts
@@ -0,0 +1,26 @@
+type PersonType = {
+    name: string;
+    age: number;
+    address: string;
+};
+
+type newType = 'name' | 'age' | 'address'
+
+type newTypeUsingKeyof = keyof PersonType
+
+
+//const a:newType='age'
+//const b:newTypeUsingKeyOf='hh'
+
+function getProperty<X, Y extends keyof X>(obj: X, key: Y) {
+    obj[key];
+}
+
+const property = getProperty({ name: "Mr. X", age: 100 }, 'age')
+
+//name({name:"Mr.X",age:100},'age')//100
+const a = {
+    name: 'Mr. X',
+    age: 100
+}
+
