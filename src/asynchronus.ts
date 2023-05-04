60c4a2b59de3b7bb3403820e1dc878737376fefc
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-05-04 11:19:57 +0600

    change in datta

--- src/asynchronus.ts
+++ src/asynchronus.ts
@@ -1,4 +1,26 @@
 //mocking
+
+//json placeholder
+
+interface ITodo {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean,
+}
+
+const getTodo = async (): Promise<ITodo> => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    return await response.json();
+};
+
+const getTodoData = async () => {
+    const result = await getTodo();
+    console.log(result);
+};
+
+getTodoData();
+
 const makePromice = (): Promise<string> => {
     return new Promise<string>((resolve, reject) => {
         const data: string = 'Data is fetched'
@@ -13,7 +35,26 @@ const makePromice = (): Promise<string> => {
 
 const makePromiceBoolean = (): Promise<boolean> => {
     return new Promise<boolean>((resolve, reject) => {
-        const data: boolean = 'Data is fetched'
+        const data: boolean = true
+        if (data) {
+            resolve(data);
+        }
+        else {
+            reject('Failed to feted data!');
+        }
+    });
+};
+
+// type DataType={
+//     data:string
+// }
+interface DataType {
+    data: string
+}
+
+const makePromiceObject = (): Promise<DataType> => {
+    return new Promise<DataType>((resolve, reject) => {
+        const data: DataType = { data: 'Data is fetched' };
         if (data) {
             resolve(data);
         }
@@ -24,8 +65,16 @@ const makePromiceBoolean = (): Promise<boolean> => {
 };
 
 
+const getPromiseObject = async (): Promise<DataType> => {
+    const data = await makePromiceObject();
+    return data
+}
 const getPromiseData = async (): Promise<string> => {
     const data = await makePromice();
+    return data
+}
+const getPromiseBoolean = async (): Promise<boolean> => {
+    const data = await makePromiceBoolean();
     return data
 }
 
