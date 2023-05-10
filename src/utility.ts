a4e0da405c10cacf1b55f35d56059999d8f52200
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-05-10 22:51:49 +0600

    cahgne

---
+++ src/utility.ts
@@ -0,0 +1,36 @@
+//Pick
+
+interface Person {
+    name: string;
+    email?: string;
+    contactNo: string;
+}
+
+type contactNo = Pick<Person, "contactNo" | 'email'>;
+
+// omit 
+type Name = Omit<Person, "email" | "contact No">;
+
+type Optional = Partial<Person>;
+type RequiredProps = Required<Person>
+
+const person: Person = {
+    name: "Persian",
+    email: 'abc@gmail.com',
+    contactNo: '1234',
+}
+
+person.name = "Forhan";
+
+//Record Type
+
+type myObj = {
+    a: string;
+    b: string;
+    c: string;
+};
+
+//using index signature
+type myObj = {
+    [key: string]: string
+}
