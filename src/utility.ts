630ebab008f2cbf5c494788f43efdbdef442114a
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-05-10 23:02:05 +0600

    clsd

--- src/utility.ts
+++ src/utility.ts
@@ -1,42 +1,46 @@
-//Pick
+// //Pick
 
-interface Person {
-    name: string;
-    email?: string;
-    contactNo: string;
-}
+// interface Person {
+//     name: string;
+//     email?: string;
+//     contactNo: string;
+// }
 
-type contactNo = Pick<Person, "contactNo" | 'email'>;
+// type contactNo = Pick<Person, "contactNo" | 'email'>;
 
-// omit 
-type Name = Omit<Person, "email" | "contact No">;
+// // omit
+// type Name = Omit<Person, "email" | "contact No">;
 
-type Optional = Partial<Person>;
-type RequiredProps = Required<Person>
+// type Optional = Partial<Person>;
+// type RequiredProps = Required<Person>
 
-const person: Person = {
-    name: "Persian",
-    email: 'abc@gmail.com',
-    contactNo: '1234',
-}
+// const person: Person = {
+//     name: "Persian",
+//     email: 'abc@gmail.com',
+//     contactNo: '1234',
+// }
 
-person.name = "Forhan";
+// person.name = "Forhan";
 
-//Record Type
+// //Record Type
 
+// // type myObj = {
+// //     a: string;
+// //     b: string;
+// //     c: string;
+// // };
+
+// //using index signature
+
+// //Record
+// type myObj = Record<'a' | 'b' | 'c' | 'd', string>
 // type myObj = {
-//     a: string;
-//     b: string;
-//     c: string;
-// };
+//     [key: string]: string
+// }
 
-//using index signature
-type myObj = {
-    [key: string]: string
-}
-
-const obj: myObj = {
-    a: "1",
-    b: "2",
-    c: "3"
-}
+// const obj: myObj = {
+//     a: "1",
+//     b: "2",
+//     c: "3",
+//     d: "4"
+// }
